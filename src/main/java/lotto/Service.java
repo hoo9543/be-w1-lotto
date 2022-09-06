efb7c6b90f5e2813957ac3c537c2851b9e407c83
aden-j <aden.j@kakaocorp.com>
2022-09-06 19:21:17 +0900

    Service 단 분리하여 logic을 처리할 수 있도록 함

--- src/main/java/lotto/Service.java
+++ src/main/java/lotto/Service.java
@@ -1,2 +1,40 @@
-package lotto;public class Service {
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Service {
+
+    public ArrayList<Integer> createLottoNumbers() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int i = 0;
+        while(i < 6){
+            int ran = (int)(Math.random()*45+1);
+            if(!result.contains(ran)){
+                result.add(ran);
+                i ++;
+            }
+        }
+
+        Collections.sort(result);
+        System.out.println(result);
+        return result;
+    }
+
+
+    public int countCorrectNumber(ArrayList<Integer> lottoNumbers, ArrayList<Integer> myNumbers){
+        Collections.sort(lottoNumbers);
+        Collections.sort(myNumbers);
+
+        int correct = 0;
+        for(int i = 0; i < 6; i++) {
+            if (myNumbers.contains(lottoNumbers.get(i))) {
+                correct ++;
+            }
+        }
+        return correct;
+        
+    }
+
 }
