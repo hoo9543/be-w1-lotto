167a3fa4a4ea2ab08d1406a4fadb6c0b5a664cea
aden-j <aden.j@kakaocorp.com>
2022-09-06 18:59:13 +0900

    input class 만들어, input 으로 받는 값들을 처리할 수 있도록 함

---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,40 @@
+package lotto;
+
+import java.util.*;
+
+public class Input {
+
+    public int inputPrice() {
+        System.out.println("구입금액을 입력해주세요.");
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
+    }
+    /*
+    public int inputLottoSize() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
+    }
+
+    public ArrayList<ArrayList<Integer>> inputBuyNumbers() {
+        System.out.println("");
+        Scanner scan = new Scanner(System.in);
+        return new ArrayList<ArrayList<Integer>>();
+    }
+    */
+    public ArrayList<Integer> inputLottoNumbers(){
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        ArrayList<Integer> lottoNumbers = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        String inputNumbers = scan.next();
+        String numbers[] = inputNumbers.split(",");
+
+        for (int i=0; i< numbers.length; i++){
+            lottoNumbers.add(Integer.parseInt(numbers[i]));
+        }
+
+        Collections.sort(lottoNumbers);
+        return lottoNumbers;
+    }
+
+}
