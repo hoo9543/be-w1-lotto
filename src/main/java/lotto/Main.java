cf1d83be5061456929f2b3d635540af61f7ae352
aden-j <aden.j@kakaocorp.com>
2022-09-06 19:34:06 +0900

    Main 에서 전체적으로 수행

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,11 +1,37 @@
 package lotto;
 
+import java.util.ArrayList;
+
 public class Main {
+    public static final int Cost = 1000;
 
     public static String getHello() {
         return "Hello";
     }
     public static void main(String[] args) {
-        System.out.println(Main.getHello());
+
+        Input input = new Input();
+        Service service = new Service();
+        Output output = new Output();
+
+        int purchaseAmount = input.inputPrice();
+        int lottoNum = purchaseAmount/Cost;
+        output.printLottoPurchase(lottoNum);
+
+        ArrayList<ArrayList<Integer>> lottoNumbers = new ArrayList<>();
+
+        for (int i=0; i<lottoNum; i++){
+            lottoNumbers.add(service.createLottoNumbers());
+        }
+
+        ArrayList<Integer> winningNumber = input.inputLottoNumbers();
+
+        for (ArrayList<Integer> lottoNumber:lottoNumbers){
+            output.counting(service.countCorrectNumber(winningNumber,lottoNumber));
+        }
+
+        output.printMatchResult();
+        output.printProfitRate(purchaseAmount);
+
     }
 }
